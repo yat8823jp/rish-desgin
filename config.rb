f3e012b0b4cad4078339385e6a227d92c69e03d0
YAT <yat@yat-net.com>
2016-09-28 13:16:17 +0900

    liquidation

--- config.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'YAT'
-client_key               '/Applications/MAMP/htdocs/rish/YAT.pem'
-validation_client_name   'chef-validator'
-validation_key           '/etc/chef-server/chef-validator.pem'
-chef_server_url          'https://YAT-no-MacBook-Pro.local:443'
-syntax_check_cache_path  '/Applications/MAMP/htdocs/rish/syntax_check_cache'
